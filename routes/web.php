a45397bbaa97c0305a0e39161e801fa834e7a77f
root <root@CentOS-8.5-64>
2022-01-08 18:05:13 +0800

    临时修改

--- routes/web.php
+++ routes/web.php
@@ -14,11 +14,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 
-Route::get('/', function () {
+Route::get('/asd', function () {
    return [1,2,2];
 });
 
-//
+
 // Route::get('/', 'StaticPagesController@home')->name('home');
 // Route::get('/help', 'StaticPagesController@help')->name('help');
 // Route::get('/about', 'StaticPagesController@about')->name('about');
