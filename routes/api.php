42acdf7adfeb39725a4b29ace416b2b2dad86d9c
林望琛 <linwc@xmpaoyou.com>
2022-01-11 10:06:05 +0800

    route提交测试

--- routes/api.php
+++ routes/api.php
@@ -26,16 +26,17 @@ Route::get('/test', 'testController@test')->name('test');
 
 
 // 用户登录(用户名，密码)
-Route::post('/login', 'oaUsersController@store')->name('login');
+Route::post('/login', 'oa\oaUsersController@store')->name('login');
 // 权限获取
-Route::get('/power', 'oaUsersController@power')->name('power');
+Route::get('/power', 'oa\oaUsersController@power')->name('power');
 /*********管理员(高权限者)操作*********/
 // 添加用户(用户名，昵称，密码，权限(json格式))
-Route::post('/addUser', 'oaUsersController@addUser')->name('addUser');
+Route::post('/addUser', 'oa\oaUsersController@addUser')->name('addUser');
 // 初始化密码(用户唯一id)
-Route::post('/initPwd', 'oaUsersController@initPwd')->name('initPwd');
+Route::post('/initPwd', 'oa\oaUsersController@initPwd')->name('initPwd');
 // 删除(用户唯一id)
-Route::post('/delUser', 'oaUsersController@delUser')->name('delUser');
+Route::post('/delUser', 'oa\oaUsersController@delUser')->name('delUser');
 // 用户权限修改(用户唯一id，权限(json格式))
-Route::post('/updateShop', 'oaUsersController@updateShop')->name('updateShop');
-
+Route::post('/updatePower', 'oa\oaUsersController@updatePower')->name('updatePower');
+// 添加店铺(店铺名称，公司名称，备注)
+Route::post('/addShop', 'oa\oaUsersController@addShop')->name('addShop');
